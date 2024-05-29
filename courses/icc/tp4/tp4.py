35f85c19649befa0911cb0de023628fca104b8c2
Roberto G. Fernández <rgfernan@gmail.com>
2024-05-29 17:12:43 -0300

    Agregado de la función de lectura de componente

--- courses/icc/tp4/tp4.py
+++ courses/icc/tp4/tp4.py
@@ -1,6 +1,13 @@
-def isfloat(num):
+def isFloat(num):
     try:
         float(num)
+        return True
+    except ValueError:
+        return False
+
+def isInt(num):
+    try:
+        int(num)
         return True
     except ValueError:
         return False
@@ -12,11 +19,33 @@ def leerPrecio():
     while not entradaValidada:
         precioComoTexto = input(prompt)
         prompt = "Ingrese un precio válido, entre 10.00 y 999.99: "
-        if isfloat(precioComoTexto):
+        if isFloat(precioComoTexto):
             precio = float(precioComoTexto)
             if precio >= 10.00 and precio <= 999.99:
                 entradaValidada = True
     return precio
 
-precio = leerPrecio()
-print(f"El precio leído es {precio}")
+def leerCodigoDeComponente(codigoDePieza):
+    entradaValidada = False
+    codigo = 0
+    prompt = "Por favor, ingrese código de componente: "
+    while not entradaValidada:
+        codigoComoTexto = input(prompt)
+        prompt = "Ingrese un código de componente válido: "
+        if isInt(codigoComoTexto):
+            codigo = int(codigoComoTexto)
+            if codigo >= 101 and codigo <= 9999:
+                if codigo%100 == codigoDePieza:
+                    entradaValidada = True
+                else:
+                    prompt = "Ingrese un código de componente válido, que termine en el código de pieza: "
+            else:
+                prompt = "Código fuera de rango. Ingrese un código de componente válido: "
+        else:
+            prompt = "Ingrese un código de componente numérico válido: "
+    return codigo
+
+#precio = leerPrecio()
+#print(f"El precio leído es {precio}")
+componente = leerCodigoDeComponente(21)
+print(f"El codigo leído es {componente}")
